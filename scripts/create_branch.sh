2681b422303fb0207353f3bfeb7ff72f00cdc98c
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2018-10-19 14:15:56 +0200

    create branch

---
+++ scripts/create_branch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MAJOR=$(echo ${1} | cut -d. -f1)
+MINOR=$(echo ${1} | cut -d. -f2)
+REVISION=$(echo ${1} | cut -d. -f3)
+
+if [[ ${REVISION} -eq 0 ]]; then
+  GIT_BRANCH=release-${MAJOR}.${MINOR}
+  echo "Creating branch $GIT_BRANCH"
+  git checkout -b ${GIT_BRANCH}
+  git push
+fi
