c1194fa34fea152c6618eb56adebccfe0e9e8203
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2018-10-22 12:20:47 +0200

    Cleanup scripts

--- scripts/branch_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-version=$1
-
-major=`echo ${version} | cut -d. -f1`
-minor=`echo ${version} | cut -d. -f2`
-
-echo "$major.$minor"
--- scripts/create_release_branch_step.sh
+++ scripts/create_release_branch_step.sh
@@ -4,8 +4,11 @@ MAJOR=$(echo ${1} | cut -d. -f1)
 MINOR=$(echo ${1} | cut -d. -f2)
 REVISION=$(echo ${1} | cut -d. -f3)
 
+GIT_URL=$2
+
 if [[ ${REVISION} -eq 0 ]]; then
   GIT_BRANCH=release-${MAJOR}.${MINOR}
-  echo "Creating branch $GIT_BRANCH"
+  echo "Creating branch ${GIT_BRANCH}"
   git checkout -b ${GIT_BRANCH}
+  git push ${GIT_URL}
 fi
